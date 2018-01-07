c2cb4cac12d9f3a1480e098ff77ca30253d41f7a
Javier RP <javiernj1@gmail.com>
2018-01-08 00:52:23 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,9 +6,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_pagedown import PageDown
 from config import config
+from flask_pymongo import PyMongo
 
 bootstrap = Bootstrap()
 mail = Mail()
+mongo = PyMongo()
 moment = Moment()
 db = SQLAlchemy()
 pagedown = PageDown()
@@ -24,6 +26,7 @@ def create_app(config_name):
 
     bootstrap.init_app(app)
     mail.init_app(app)
+    mongo.init_app(app)
     moment.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
