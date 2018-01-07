5d699b79fb290cfdbc51669f017085e22ec88aad
Javier RP <javiernj1@gmail.com>
2018-01-07 20:09:44 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -86,7 +86,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-    register_date = db.Column(db.DateTime(), default=datetime.utcnow)
+    register_date = db.Column(db.DateTime())
     password_hash = db.Column(db.String(128))
     confirmed = db.Column(db.Boolean, default=False)
     name = db.Column(db.String(64))
