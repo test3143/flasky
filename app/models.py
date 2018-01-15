6f3bd310160e47ebd6f12f6a0eb462ed79b27aea
test3143 <35340859+test3143@users.noreply.github.com>
2018-01-15 23:32:58 +0000

    Update models.py

--- app/models.py
+++ app/models.py
@@ -95,6 +95,7 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     confirmed = db.Column(db.Boolean, default=False)
     name = db.Column(db.String(64))
+    paid = db.Column(db.Boolean, default=False)
     location = db.Column(db.String(64))
     about_me = db.Column(db.Text())
     member_since = db.Column(db.DateTime(), default=datetime.utcnow)
